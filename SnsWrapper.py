6fe85d0a35641e46fe910d3df5b4ceb22297a868
Sopho Kevlishvili <sk4698@columbia.edu>
2023-02-16 18:28:31 -0500

    SNS wrapper class

---
+++ SnsWrapper.py
@@ -0,0 +1,137 @@
+import json
+import logging
+from botocore.exceptions import ClientError
+from botocore.config import Config
+
+logger = logging.getLogger(__name__)
+
+class SnsWrapper:
+    def __init__(self, sns_resource):
+        self.sns_resource = sns_resource
+
+    def create_topic(self, name):
+        try:
+            topic = self.sns_resource.create_topic(Name=name)
+            logger.info("Created topic %s with ARN %s.", name, topic.arn)
+        except ClientError:
+            logger.exception("Couldn't create topic %s.", name)
+            raise
+        else:
+            return topic
+
+    def list_topics(self):
+        try:
+            topics_iter = self.sns_resource.topics.all()
+            logger.info("Got topics.")
+        except ClientError:
+            logger.exception("Couldn't get topics.")
+            raise
+        else:
+            return topics_iter
+
+    @staticmethod
+    def delete_topic(topic):
+        try:
+            topic.delete()
+            logger.info("Deleted topic %s.", topic.arn)
+        except ClientError:
+            logger.exception("Couldn't delete topic %s.", topic.arn)
+            raise
+
+    @staticmethod
+    def subscribe(topic, protocol, endpoint):
+        try:
+            subscription = topic.subscribe(
+                Protocol=protocol, Endpoint=endpoint, ReturnSubscriptionArn=True)
+            logger.info("Subscribed %s %s to topic %s.", protocol, endpoint, topic.arn)
+        except ClientError:
+            logger.exception(
+                "Couldn't subscribe %s %s to topic %s.", protocol, endpoint, topic.arn)
+            raise
+        else:
+            return subscription
+
+    def list_subscriptions(self, topic=None):
+        try:
+            if topic is None:
+                subs_iter = self.sns_resource.subscriptions.all()
+            else:
+                subs_iter = topic.subscriptions.all()
+            logger.info("Got subscriptions.")
+        except ClientError:
+            logger.exception("Couldn't get subscriptions.")
+            raise
+        else:
+            return subs_iter
+
+    @staticmethod
+    def add_subscription_filter(subscription, attributes):
+        try:
+            att_policy = {key: [value] for key, value in attributes.items()}
+            subscription.set_attributes(
+                AttributeName='FilterPolicy', AttributeValue=json.dumps(att_policy))
+            logger.info("Added filter to subscription %s.", subscription.arn)
+        except ClientError:
+            logger.exception(
+                "Couldn't add filter to subscription %s.", subscription.arn)
+            raise
+
+    @staticmethod
+    def delete_subscription(subscription):
+        try:
+            subscription.delete()
+            logger.info("Deleted subscription %s.", subscription.arn)
+        except ClientError:
+            logger.exception("Couldn't delete subscription %s.", subscription.arn)
+            raise
+
+    def publish_text_message(self, phone_number, message):
+        try:
+            response = self.sns_resource.meta.client.publish(
+                PhoneNumber=phone_number, Message=message)
+            message_id = response['MessageId']
+            logger.info("Published message to %s.", phone_number)
+        except ClientError:
+            logger.exception("Couldn't publish message to %s.", phone_number)
+            raise
+        else:
+            return message_id
+
+    @staticmethod
+    def publish_message(topic, message, attributes):
+        try:
+            att_dict = {}
+            for key, value in attributes.items():
+                if isinstance(value, str):
+                    att_dict[key] = {'DataType': 'String', 'StringValue': value}
+                elif isinstance(value, bytes):
+                    att_dict[key] = {'DataType': 'Binary', 'BinaryValue': value}
+            response = topic.publish(Message=message, MessageAttributes=att_dict)
+            message_id = response['MessageId']
+            logger.info(
+                "Published message with attributes %s to topic %s.", attributes,
+                topic.arn)
+        except ClientError:
+            logger.exception("Couldn't publish message to topic %s.", topic.arn)
+            raise
+        else:
+            return message_id
+
+    @staticmethod
+    def publish_multi_message(
+            topic, subject, default_message, sms_message, email_message):
+        try:
+            message = {
+                'default': default_message,
+                'sms': sms_message,
+                'email': email_message
+            }
+            response = topic.publish(
+                Message=json.dumps(message), Subject=subject, MessageStructure='json')
+            message_id = response['MessageId']
+            logger.info("Published multi-format message to topic %s.", topic.arn)
+        except ClientError:
+            logger.exception("Couldn't publish message to topic %s.", topic.arn)
+            raise
+        else:
+            return message_id
